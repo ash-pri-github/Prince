d1e01149268caa2fe7bff49b77d6494ec65adf3f
sroy0 <sroyintp@gmail.com>
2024-12-06 16:10:54 +0530

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,181 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: 'Home',
+    href: '#home',
+  },
+  {
+    id: 2,
+    name: 'About',
+    href: '#about',
+  },
+  {
+    id: 3,
+    name: 'Work',
+    href: '#work',
+  },
+  {
+    id: 4,
+    name: 'Contact',
+    href: '#contact',
+  },
+];
+
+export const clientReviews = [
+  {
+    id: 1,
+    name: 'Indian Institute of Engineering Science and Technology, Shibpur',
+    position: 'B.Tech in Electrical Engineering, CGPA-7.33',
+    img: 'assets/review1.png',
+    review:
+      '',
+  },
+  {
+    id: 2,
+    name: 'Plus Two High School Shivaji Nagar, 2016-2019',
+    position: '*Secondary Education(Score 80%)-(2016-2017) *Senior Secondary Education(2017-2019):- Score 79.2%',
+    img: 'assets/review2.png',
+    review:
+      '',
+  },
+];
+export const myProjects = [
+  {
+    title: 'BiteBliss- Online Restaurant Ordering Platform',
+    desc: 'Architected and implemented an innovative restaurant food ordering platform using ReactJS. Delivered a seamless and intuitive user experience with real-time data synchronisation.',
+    subdesc:
+      'Increased order accuracy by 30% and improved customer satisfaction.',
+    href: 'https://react-food-app-8dbdc.web.app/',
+    texture: '/textures/project/project2.mp4',
+    logo: '/assets/project-logo2.png',
+    logoStyle: {
+      backgroundColor: '#13202F',
+      border: '0.2px solid #17293E',
+      boxShadow: '0px 0px 60px 0px #2F6DB54D',
+    },
+    spotlight: '/assets/spotlight2.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+    ],
+  },
+  {
+    title: 'Horizon - Online Banking Platform',
+    desc: 'Horizon is a comprehensive online banking platform that offers users a centralized finance management dashboard. It allows users to connect multiple bank accounts, monitor real-time transactions, and seamlessly transfer money to other users.',
+    subdesc:
+      'Built with Next.js 14 Appwrite, Dwolla and Plaid, Horizon ensures a smooth and secure banking experience, tailored to meet the needs of modern consumers.',
+    href: 'https://link',
+    texture: '/textures/project/project4.mp4',
+    logo: '/assets/project-logo4.png',
+    logoStyle: {
+      backgroundColor: '#0E1F38',
+      border: '0.2px solid #0E2D58',
+      boxShadow: '0px 0px 60px 0px #2F67B64D',
+    },
+    spotlight: '/assets/spotlight4.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Imaginify - AI Photo Manipulation App',
+    desc: 'Imaginify is a groundbreaking Software-as-a-Service application that empowers users to create stunning photo manipulations using AI technology. With features like AI-driven image editing, a payments system, and a credits-based model.',
+    subdesc:
+      'Built with Next.js 14, Cloudinary AI, Clerk, and Stripe, Imaginify combines cutting-edge technology with a user-centric approach. It can be turned into a side income or even a full-fledged business.',
+    href: 'https:fill link',
+    texture: '/textures/project/project5.mp4',
+    logo: '/assets/project-logo5.png',
+    logoStyle: {
+      backgroundColor: '#1C1A43',
+      border: '0.2px solid #252262',
+      boxShadow: '0px 0px 60px 0px #635BFF4D',
+    },
+    spotlight: '/assets/spotlight5.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+  };
+};
+
+export const workExperiences = [
+  {
+    id: 1,
+    name: 'Afcons',
+    pos: 'Graduate Trainee Engineer',
+    duration: '2024 - Present',
+    title: "I assist in project planning, design, and client relation. My responsibilities include supporting senior engineers with data analysis, design preparation, and site observation, while maintaining accurate project documentation. I actively participate in training programs to enhance my technical skills and gain practical experience in engineering and project management.",
+    icon: 'https://upload.wikimedia.org/wikipedia/en/e/ed/AFCONS_logo.svg',
+    animation: 'victory',
+  },
+  {
+    id: 2,
+    name: 'Celebal Technologies',
+    pos: 'Web Developer Intern',
+    duration: 'May 2023 - July 202',
+    title: " Created  efficient  and  engaging  user  interfaces  as  a  React.js Developer Intern. Utilized  JSX  for  reusable  components,  integrated  React  Router for  smooth  navigation,  and  implemented  Redux  for  streamlined data management. Conducted  usability  testing  and  played  a  significant  role  in completing six impactful projects.",
+    icon: 'https://upload.wikimedia.org/wikipedia/commons/9/90/Wiki-tech-logo-js.svg',
+    animation: 'clapping',
+  },
+];
